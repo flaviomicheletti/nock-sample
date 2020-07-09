69794e920d7be4401fe35dc2fcf16c11af86de92
Flávio Micheletti <flaviomicheletti@hotmail.com>
2020-07-08 21:47:17 -0300

    add Jest and Axios

--- api.js
+++ api.js
@@ -1,25 +1,42 @@
-var request = require("request");
+const axios = require('axios');
+const adapter = require('axios/lib/adapters/http');
 
-var myapi = request.defaults({ baseUrl: "http://jservice.io/api/" });
+// config
+const myapi = axios.create({
+  baseURL: "http://jservice.io/api/",
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+  },
+  adapter: adapter
+});
 
 module.exports = {
 
   getCategory: function (id, cb) {
 
-    myapi.get("/category?id=" + id, function (error, response, body) {
-      var category = JSON.parse(body);
-      // console.log(category.clues_count);
-      cb(category.title);
-    });
+    myapi.get('/category?id=' + id)
+      .then(function (response) {
+        // console.log(response);
+        var category = response.data;
+        // console.log(category.clues_count);
+        cb(category.title);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
 
   },
 
   getRandomQuestion: function (cb) {
 
-    myapi.get("/random", function (error, response, body) {
-      var question = JSON.parse(body)[0];
-      cb(question.question);
-    });
+    myapi.get('/random' )
+      .then(function (response) {
+        var question = response.data[0];
+        cb(question.question);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
 
   }
 
